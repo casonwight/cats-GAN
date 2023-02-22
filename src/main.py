60d904a77d19f2fdec6df8e4a265f6b6f5d23baf
Cason Wight <cason.wight@gmail.com>
2023-02-21 20:04:54 -0700

    setting device to cuda if available instead of windows

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import torch
-import torch_directml
 from training import GanTrainer
 
 def main():
-    gpu_device = torch_directml.device()
-    gan = GanTrainer(device=gpu_device, n_epochs=50, save_every=300, val_every=100)
+    device = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    gan = GanTrainer(device=device, n_epochs=50, save_every=300, val_every=100)
     gan.train()
 
 if __name__ == "__main__":
